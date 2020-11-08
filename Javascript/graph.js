f9cb966d725e7c5c41e93d6e6e318d5663ac3577
587763 <69586003+587763@users.noreply.github.com>
2020-11-08 15:58:11 +0100

    tabeller so blir hidden/visible
    we did it bois

--- Javascript/graph.js
+++ Javascript/graph.js
@@ -2,5 +2,10 @@
 function showImage(name) {
 
 		document.getElementById("graph").src = "bilder/"+ name + ".png";
-		document.getElementById("graph").className = "visible";
+		document.getElementById("graph").className = "visibleA";
+		document.getElementById("50").className = "hiddenB";
+		document.getElementById("150").className = "hiddenB";
+		document.getElementById("250").className = "hiddenB";
+		document.getElementById(name).className = "visibleB";
 		}
+		
